79635640316888fa5a852c18c91bec7293f58729
Dylan <dylc2770@gmail.com>
2020-07-04 22:44:45 +0200

    remove second article

--- src/data.js
+++ src/data.js
@@ -22,14 +22,4 @@ export const posts = [
     body:
       "Ever been in a room and felt like something was missing? Perhaps if felt slightly bare and inunviting. I've got some simple tips to help you make any room feel complete.",
   },
-  {
-    id: 2,
-    date: "28 Jun 2020",
-    image: "drawers.jpg",
-    authorName: "Michelle Appleton",
-    title:
-      "Shift the overall look and feel by adding these wonderful touches to furniture in your home",
-    body:
-      "Ever been in a room and felt like something was missing? Perhaps if felt slightly bare and inunviting. I've got some simple tips to help you make any room feel complete.",
-  },
 ];
